5e7af4fdc05a3c9731b4bd685b9abd659b0284e6
flacofalso <aravena.laflorida@gmail.com>
2026-01-11 14:29:32 -0300

    Agregar Google Analytics

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1893105816"
+    serviceWorkerVersion: "940095416"
   }
 });
